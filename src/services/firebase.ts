1ce220d25124986c939a92d0d5931a07c22efb8e
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-09-28 11:19:04 -0300

    fire base config

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import firebase from 'firebase/'
+
+import 'firebase/auth'
+import 'firebase/database'
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_DATABASE_URL,
+    storageBucket: process.env.REACT_APP_DATABASE_URL,
+    messagingSenderId: "166307600000",
+    appId: process.env.REACT_APP_DATABASE_URL
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const database = firebase.database()
