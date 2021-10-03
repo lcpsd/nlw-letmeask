7daff9822779f2779b0ac8f9c832bab8dfb4bfbb
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-10-03 14:56:21 -0300

    Basic question listing and styles

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import logoImg from '../assets/images/logo.svg'
 import { Button } from '../components/Button'
+import { Question } from '../components/Question'
 import { RoomCode } from '../components/RoomCode'
 import { useAuth } from '../hooks/useAuth'
 import { database } from '../services/firebase'
@@ -23,7 +24,11 @@ type FirebaseQuestions = Record<string, {
     isHighLighted: boolean
 }>
 
-type Questions = {
+type QuestionType = {
+    author: {
+        name: string;
+        avatar: string;
+    }
     id: string;
     content: string;
     isAnswered: boolean;
@@ -32,19 +37,21 @@ type Questions = {
 
 export function Room() {
 
+    
     const {user} = useAuth()
-
+    
     const params = useParams<RoomParams>()
     const roomId = params.id
     
     const [newQuestion, setNewQuestion] = useState('')
-    const [questions, setQuestions] = useState<Questions[]>([])
+    const [questions, setQuestions] = useState<QuestionType[]>([])
     const [title, setTitle] = useState('')
-
+    
+    console.log(questions)
     useEffect(()=> {
         const roomRef = database.ref(`rooms/${roomId}`)
 
-        roomRef.once('value', room => {
+        roomRef.on('value', room => {
             const databaseRoom = room.val()
 
             const FirebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
@@ -52,13 +59,15 @@ export function Room() {
             const parsedQuestions = Object.entries(FirebaseQuestions).map(([key, value]) => {
                 return {
                     id: key,
+                    author: value.author,
                     content: value.content,
                     isHighLighted: value.isHighLighted,
-                    isAnswered: value.isAnswered
+                    isAnswered: value.isAnswered,
                 }
             })
             
             setTitle(databaseRoom.title)
+            //@ts-ignore
             setQuestions(parsedQuestions)
         } )
 
@@ -104,9 +113,22 @@ export function Room() {
             <main>
                 <div className="room-title">
                     <h1>Sala {title}</h1>
+
                     {
-                        questions.length > 0 && 
+                        questions.length < 1 && 
+                        <i></i>
+                    }
+
+                    {
+                        questions.length > 1 && 
                         <span>{questions.length} perguntas</span>
+                    }
+
+                    {
+                        questions.length === 1 && questions.length < 2 ?
+                        <span>{questions.length} pergunta</span> 
+                        :
+                        <i></i>
                     }
                 </div>
 
@@ -120,7 +142,7 @@ export function Room() {
 
                         {user ? (
                             <div className="user-info">
-                                <img src={user.avatar}/>
+                                <img src={user.avatar} alt=''/>
                                 <span>{user.name}</span>
                             </div>
                         ) : (
@@ -130,6 +152,20 @@ export function Room() {
                         <Button type="submit" disabled={!user}>Enviar pergunta</Button>
                     </div>
                 </form>
+
+                <div className="question-lis">
+                    {
+                        questions.map(question => {
+                            return(
+                                <Question
+                                    content={question.content}
+                                    author={question.author}
+                                    key={question.id}
+                                />
+                            )
+                        })
+                    }
+                </div>
             </main>
         </div>
     )
